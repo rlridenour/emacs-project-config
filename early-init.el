51f4b297c11fb49f0b63218831db427feaebc3ff
Randy Ridenour <rlridenour@gmail.com>
2025-06-07 05:22:24 -0500

    Add performance section.

--- early-init.el
+++ early-init.el
@@ -3,6 +3,41 @@
 (setq package-enable-at-startup nil)
 (setq inhibit-default-init nil)
 
+(setq gc-cons-threshold most-positive-fixnum
+	gc-cons-percentage 1)
+
+(defun +gc-after-focus-change ()
+  "Run GC when frame loses focus."
+  (run-with-idle-timer
+   5 nil
+   (lambda () (unless (frame-focus-state) (garbage-collect)))))
+
+(setq max-lisp-eval-depth 10000)
+
+(defun +reset-init-values ()
+  (run-with-idle-timer
+   1 nil
+   (lambda ()
+     (setq file-name-handler-alist default-file-name-handler-alist
+	     gc-cons-percentage 0.1
+	     gc-cons-threshold 100000000)
+     (let ((inhibit-message t))
+	 (message "gc-cons-threshold & file-name-handler-alist restored"))
+     (message "ELPACA LOADING COMPLETE: %s" (emacs-init-time))
+     (when (boundp 'after-focus-change-function)
+	 (add-function :after after-focus-change-function #'+gc-after-focus-change)))))
+
+(with-eval-after-load 'elpaca
+  (add-hook 'elpaca-after-init-hook '+reset-init-values))
+
+(customize-set-variable 'native-comp-speed 2)
+(customize-set-variable 'native-comp-deferred-compilation t)
+
+(setq native-comp-async-report-warnings-errors nil)
+
+(defvar default-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
 (setq user-full-name "Randy Ridenour"
 	user-mail-address "rlridenour@fastmail.com")
 
